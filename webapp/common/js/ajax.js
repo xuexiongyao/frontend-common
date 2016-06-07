a11178ded173d7b86bea7d708e972cc5f999277a
zhuwei <125427446@qq.com.cn>
2016-06-07 10:12:48 +0800

    更改源码jquery,修改ajax.js

--- webapp/common/js/ajax.js
+++ webapp/common/js/ajax.js
@@ -1,3 +1,21 @@
+//f.statusText
+function setuErrorHandler(msg,errFun){
+    if(!msg.responseText){
+        errFun(msg);
+    }
+    var errorJsonStr=msg.responseText.match(/var errorInfo=([\w\W]+);/)[1];
+    if(errorJsonStr!=undefined){
+        var errorJson=eval('('+errorJsonStr+')');
+        if(errorJson.errorCode=="-202"){
+            window.top.location.href="../loginpage";
+        }else{
+            window.top.location.href="../loginpage";
+            alert(errorJson.message);
+        }
+    }else{
+        errFun(msg);
+    }
+}
 function postQuery(action, data, sucFun, errFun) {
     var loader;
     try{
@@ -14,20 +32,7 @@ function postQuery(action, data, sucFun, errFun) {
 
     }
     var errCallBack=function(msg){
-        if(!msg.responseText){
-            errFun(msg);
-        }
-        var errorJsonStr=msg.responseText.match(/var errorInfo=([\w\W]+);/)[1];
-        if(errorJsonStr!=undefined){
-            var errorJson=eval('('+errorJsonStr+')');
-            if(errorJson.errorCode=="-102"){
-                window.top.location.href="../index.html";
-            }else{
-                alert(errorJson.message);
-            }
-        }else{
-            errFun(msg);
-        }
+        setuErrorHandler(msg,errFun);
     }
 
     var sucCallBack = function(result){
