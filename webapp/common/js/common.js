8abadabb315395f2d7efb8f8dbbfd35ac19181e3
zhuwei <125427446@qq.com.cn>
2016-06-28 17:26:06 +0800

    完善common.js

--- webapp/common/js/common.js
+++ webapp/common/js/common.js
@@ -574,30 +574,30 @@ function openUrlForm(options, btn_diy) {
 function openDivForm(options, btn_diy) {
     /*参数使用说明举例
      openDivForm({
-     id: 'div_id', //页面上div的id,将div设置为display:none,在div中设置好form属性,自动提交第一个form
-     title: '表单提交',
-     width: 800,
-     height: 200,
-     top: 200,
-     beforeSubmit: function () {
-     }, //return false,阻止提交
-     afterSubmit: function (data) {
-     }, //提交成功,data为返回的数据
-     onClose: function () {
-     },             //关闭时提交的函数
-     }, [                     //以下为按钮添加配置,不传值为默认,传递[]时,清除所有按钮
-     {
-     text: '确定',
-     handler: function () {
-     $('#div_id').dialog('close');
-     }
-     }, {
-     text: '关闭',
-     handler: function () {
-     $('#div_id').dialog('close');
-     }
-     },
-     ]);*/
+        id: 'div_id', //页面上div的id,将div设置为display:none,在div中设置好form属性,自动提交第一个form
+        title: '表单提交',
+        width: 800,
+        height: 200,
+        top: 200,
+        beforeSubmit: function () {
+        }, //return false,阻止提交
+        afterSubmit: function (data) {
+        }, //提交成功,data为返回的数据
+        onClose: function () {
+        },             //关闭时提交的函数
+    }, [                     //以下为按钮添加配置,不传值为默认,传递[]时,清除所有按钮
+        {
+            text: '确定',
+            handler: function () {
+                $('#div_id').dialog('close');
+            }
+        }, {
+            text: '关闭',
+            handler: function () {
+                $('#div_id').dialog('close');
+            }
+        },
+    ]);*/
     var dlg_id = options.id;
     var dlg_div = $('#' + dlg_id);
     var defualt_beforeSubmit = function () {
