04c6fa7d6e9a144232510a478bf6c79ca8232edf
zhuwei <125427446@qq.com.cn>
2016-07-09 17:34:33 +0800

    静态资源版本升级

--- webapp/common/js/includeCSS.js
+++ webapp/common/js/includeCSS.js
@@ -1,7 +1,7 @@
 /*css*/
-document.write('<link rel="stylesheet" href="http://static.jwzh.com:7777/jwzh/common/easyuiDiy/easyui.css?v=1.0.0.0">');
-document.write('<link rel="stylesheet" href="http://static.jwzh.com:7777/jwzh/common/easyuiDiy/icon.css?v=1.0.0.0">');
-document.write('<link rel="stylesheet" href="http://static.jwzh.com:7777/jwzh/common/easyuiDiy/font-awesome.min.css?v=1.0.0.0">');
-document.write('<link rel="stylesheet" href="http://static.jwzh.com:7777/jwzh/common/datepicker/skin/christ/datepicker.css?v=1.0.0.0">');
-document.write('<link rel="stylesheet" href="http://static.jwzh.com:7777/jwzh/stylesheets/newItemStyle.css?v=1.0.0.0">');
-document.write('<link rel="stylesheet" href="http://static.jwzh.com:7777/jwzh/stylesheets/common.css?v=1.0.0.0">');
+document.write('<link rel="stylesheet" href="http://static.jwzh.com:7777/jwzh/common/easyuiDiy/easyui.css?v=1.0.0.1">');
+document.write('<link rel="stylesheet" href="http://static.jwzh.com:7777/jwzh/common/easyuiDiy/icon.css?v=1.0.0.1">');
+document.write('<link rel="stylesheet" href="http://static.jwzh.com:7777/jwzh/common/easyuiDiy/font-awesome.min.css?v=1.0.0.1">');
+document.write('<link rel="stylesheet" href="http://static.jwzh.com:7777/jwzh/common/datepicker/skin/christ/datepicker.css?v=1.0.0.1">');
+document.write('<link rel="stylesheet" href="http://static.jwzh.com:7777/jwzh/stylesheets/newItemStyle.css?v=1.0.0.1">');
+document.write('<link rel="stylesheet" href="http://static.jwzh.com:7777/jwzh/stylesheets/common.css?v=1.0.0.1">');
--- webapp/common/js/includeJS.js
+++ webapp/common/js/includeJS.js
@@ -1,13 +1,13 @@
 /*js*/
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/easyui/jquery.easyui.extend.js?v=1.0.0.0"></script>');
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/easyui/jquery.easyui.extend.validatebox.js?v=1.0.0.0"></script>');
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/js/tools.js?v=1.0.0.0"></script>');
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/js/business.tools.js?v=1.0.0.0"></script>');
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/js/windowTopPage.js?v=1.0.0.0"></script>');
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/js/messenger.js?v=1.0.0.0"></script>');
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/js/common.js?v=1.0.0.0"></script>');
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/js/ajax.js?v=1.0.0.0"></script>');
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/datepicker/WdatePicker.js?v=1.0.0.0"></script>');
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/easyui/jquery.dragsort-0.5.2.js?v=1.0.0.0"></script>');
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/easyui/jquery.easyui.extend.tabs.js?v=1.0.0.0"></script>');
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/easyui/jquery.form.js?v=1.0.0.0"></script>');
+document.write('<script src="http://static.jwzh.com:7777/jwzh/common/easyui/jquery.easyui.extend.js?v=1.0.0.1"></script>');
+document.write('<script src="http://static.jwzh.com:7777/jwzh/common/easyui/jquery.easyui.extend.validatebox.js?v=1.0.0.1"></script>');
+document.write('<script src="http://static.jwzh.com:7777/jwzh/common/js/tools.js?v=1.0.0.1"></script>');
+document.write('<script src="http://static.jwzh.com:7777/jwzh/common/js/business.tools.js?v=1.0.0.1"></script>');
+document.write('<script src="http://static.jwzh.com:7777/jwzh/common/js/windowTopPage.js?v=1.0.0.1"></script>');
+document.write('<script src="http://static.jwzh.com:7777/jwzh/common/js/messenger.js?v=1.0.0.1"></script>');
+document.write('<script src="http://static.jwzh.com:7777/jwzh/common/js/common.js?v=1.0.0.1"></script>');
+document.write('<script src="http://static.jwzh.com:7777/jwzh/common/js/ajax.js?v=1.0.0.1"></script>');
+document.write('<script src="http://static.jwzh.com:7777/jwzh/common/datepicker/WdatePicker.js?v=1.0.0.1"></script>');
+document.write('<script src="http://static.jwzh.com:7777/jwzh/common/easyui/jquery.dragsort-0.5.2.js?v=1.0.0.1"></script>');
+document.write('<script src="http://static.jwzh.com:7777/jwzh/common/easyui/jquery.easyui.extend.tabs.js?v=1.0.0.1"></script>');
+document.write('<script src="http://static.jwzh.com:7777/jwzh/common/easyui/jquery.form.js?v=1.0.0.1"></script>');
