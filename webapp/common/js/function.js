43ba4d4a8f13303380e6f577aaf5fbb022cc4dc3
christ <1010468647@qq.com>
2016-08-24 09:36:10 +0800

    新增页面之间获取参数函数

--- webapp/common/js/function.js
+++ webapp/common/js/function.js
@@ -1014,3 +1014,23 @@ function comboAutoComplete(combobox_id,url){
         }
     });
 }
+
+//html页面参数获取
+function getParamLinkUrl(paramName) {
+    var paramValue = "";
+    var isFound = false;
+    if (this.location.search.indexOf("?") == 0 && this.location.search.indexOf("=") > 1) {
+        var arrSource = unescape(this.location.search).substring(1, this.location.search.length).split("&");
+        i = 0;
+        while (i < arrSource.length && !isFound) {
+            if (arrSource[i].indexOf("=") > 0) {
+                if (arrSource[i].split("=")[0].toLowerCase() == paramName.toLowerCase()) {
+                    paramValue = arrSource[i].split("=")[1];
+                    isFound = true;
+                }
+            }
+            i++;
+        }
+    }
+    return paramValue;
+}
