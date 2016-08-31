1cedd75658510bab1d2f2b4c79ce70c5b4ca20e7
zhuwei <125427446@qq.com.cn>
2016-08-31 18:01:35 +0800

    js,css资源版本管理,   每次修改includeJS.js中的版本号即可

--- webapp/common/js/common.js
+++ webapp/common/js/common.js
@@ -5,8 +5,19 @@
 var current_tab_id = null;
 var return_tab_id = null;
 var staticPath = 'http://static.jwzh.com:7777/jwzh';
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/js/function.js?v=1.0.0.1"></script>');
+document.write('<script src="'+staticPath+'/common/js/function.js?v=1.0.0.1"></script>');
 $(function () {
+    //版本管理获取最新版本号进行对比,如果变化则强制刷新页面
+    var test_version = window.localStorage.jwzhVersion;
+    var pathname = location.pathname;
+    console.log(localStorage[pathname],test_version);
+    if(window.localStorage[pathname] != test_version){
+        window.localStorage[pathname] = test_version;
+        alert('调试: 版本已经升级,页面将重新加载一次最新资源文件');
+        location.reload(true);
+    }
+
+
     //加载完成后,发送消息到父框架获取当前TabID和上一个TabID
     if (typeof window_type == 'undefined') {
         try {
--- webapp/common/js/includeJS.js
+++ webapp/common/js/includeJS.js
@@ -1,16 +1,18 @@
 /*js*/
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/easyui/jquery.easyui.extend.js?v=1.0.0.1"></script>');
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/easyui/jquery.easyui.extend.validatebox.js?v=1.0.0.1"></script>');
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/js/tools.js?v=1.0.0.1"></script>');
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/js/business.tools.js?v=1.0.0.1"></script>');
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/js/windowTopPage.js?v=1.0.0.1"></script>');
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/js/messenger.js?v=1.0.0.1"></script>');
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/js/common.js?v=1.0.0.1"></script>');
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/js/ajax.js?v=1.0.0.1"></script>');
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/datepicker/WdatePicker.js?v=1.0.0.1"></script>');
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/easyui/jquery.dragsort-0.5.2.js?v=1.0.0.1"></script>');
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/easyui/jquery.linkCss.js?v=1.0.0.1"></script>');
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/easyui/jquery.easyui.extend.tabs.js?v=1.0.0.1"></script>');
-document.write('<script src="http://static.jwzh.com:7777/jwzh/common/easyui/jquery.form.js?v=1.0.0.1"></script>');
+var jwzhVersion = '1.0.0.0'; //手动修改版本号
+window.localStorage.jwzhVersion = jwzhVersion;
+document.write('<script src="http://static.jwzh.com:7777/jwzh/common/easyui/jquery.easyui.extend.js?v='+jwzhVersion+'"></script>');
+document.write('<script src="http://static.jwzh.com:7777/jwzh/common/easyui/jquery.easyui.extend.validatebox.js?v='+jwzhVersion+'"></script>');
+document.write('<script src="http://static.jwzh.com:7777/jwzh/common/js/tools.js?v='+jwzhVersion+'"></script>');
+document.write('<script src="http://static.jwzh.com:7777/jwzh/common/js/business.tools.js?v='+jwzhVersion+'"></script>');
+document.write('<script src="http://static.jwzh.com:7777/jwzh/common/js/windowTopPage.js?v='+jwzhVersion+'"></script>');
+document.write('<script src="http://static.jwzh.com:7777/jwzh/common/js/messenger.js?v='+jwzhVersion+'"></script>');
+document.write('<script src="http://static.jwzh.com:7777/jwzh/common/js/common.js?v='+jwzhVersion+'"></script>');
+document.write('<script src="http://static.jwzh.com:7777/jwzh/common/js/ajax.js?v='+jwzhVersion+'"></script>');
+document.write('<script src="http://static.jwzh.com:7777/jwzh/common/datepicker/WdatePicker.js?v='+jwzhVersion+'"></script>');
+document.write('<script src="http://static.jwzh.com:7777/jwzh/common/easyui/jquery.dragsort-0.5.2.js?v='+jwzhVersion+'"></script>');
+document.write('<script src="http://static.jwzh.com:7777/jwzh/common/easyui/jquery.linkCss.js?v='+jwzhVersion+'"></script>');
+document.write('<script src="http://static.jwzh.com:7777/jwzh/common/easyui/jquery.easyui.extend.tabs.js?v='+jwzhVersion+'"></script>');
+document.write('<script src="http://static.jwzh.com:7777/jwzh/common/easyui/jquery.form.js?v='+jwzhVersion+'"></script>');
 document.write('<script src="http://static.jwzh.com:7777/jwzh/common/map/map.js"></script>');
 document.write('<script src="http://static.jwzh.com:7777/jwzh/common/map/mapApi.js"></script>');
