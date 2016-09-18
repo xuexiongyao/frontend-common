1031249c676b2bbeb8ccd96dd70257503fdb9dd4
zhuwei <125427446@qq.com.cn>
2016-09-18 14:16:54 +0800

    静态资源优化

--- webapp/common/js/common.js
+++ webapp/common/js/common.js
@@ -5,17 +5,6 @@
 var current_tab_id = null;
 var return_tab_id = null;
 $(function () {
-    //版本管理获取最新版本号进行对比,如果变化则强制刷新页面
-    var pathname = location.pathname;
-    console.log(pathname,getCookie(pathname),jwzhVersion);
-    if(jwzhVersion){
-        if(!getCookie(pathname) || getCookie(pathname) != jwzhVersion){
-            setCookie(pathname,jwzhVersion,300);
-            console.log('测试版本提示: 版本已经升级,页面将重新加载一次最新资源文件..版本号:'+jwzhVersion);
-            location.reload(true);
-        }
-    }
-
     //加载完成后,发送消息到父框架获取当前TabID和上一个TabID
     if (typeof window_type == 'undefined') {
         try {
--- webapp/common/js/includeCSS.js
+++ webapp/common/js/includeCSS.js
@@ -1,9 +1,19 @@
 
 /******静态资源路径,和版本号配置********/
 var staticPath = 'http://static.jwzh.com:7777/jwzh';
-var jwzhVersion = '1.0.0.t10';
-console.log('静态资源链接和版本号:',staticPath,jwzhVersion);
-/***END***/
+var jwzhVersion = '1.0.0.t11';
+//console.log('静态资源链接和版本号:',staticPath,jwzhVersion);
+/*********END***********************/
+
+//版本管理获取最新版本号进行对比,如果变化则强制刷新页面
+var pathname = location.pathname;
+if(jwzhVersion){
+    if(!getCookie(pathname) || getCookie(pathname) != jwzhVersion){
+        setCookie(pathname,jwzhVersion,300);
+        console.log('测试版本提示: 版本已经升级,页面将重新加载一次最新资源文件..版本号:'+jwzhVersion);
+        location.reload(true);
+    }
+}
 
 
 /*css*/
@@ -13,3 +23,21 @@ document.write('<link rel="stylesheet" href="'+staticPath+'/common/easyuiDiy/fon
 document.write('<link rel="stylesheet" href="'+staticPath+'/common/datepicker/skin/christ/datepicker.css?v='+jwzhVersion+'">');
 document.write('<link rel="stylesheet" href="'+staticPath+'/stylesheets/newItemStyle.css?v='+jwzhVersion+'">');
 document.write('<link rel="stylesheet" href="'+staticPath+'/stylesheets/common.css?v='+jwzhVersion+'">');
+
+
+//获取cookie值
+function getCookie(name){
+    var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+    if(arr=document.cookie.match(reg))
+        return unescape(arr[2]);
+    else
+        return null;
+}
+
+//写入到Cookie
+//c_name:cookie名称,value:cookie值,expiredays:过期天数
+function setCookie(c_name,value,expiredays){
+    var exdate=new Date();
+    exdate.setDate(exdate.getDate()+expiredays);
+    document.cookie=c_name+ "=" +escape(value)+	((expiredays==null) ? "" : ";expires="+exdate.toGMTString());
+}
