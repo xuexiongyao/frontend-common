fb8d057ef23c6688bb8d2ef31751285c99a2af68
zhuwei <125427446@qq.com.cn>
2017-01-17 15:51:55 +0800

    版本号修改

--- webapp/common/js/includeCSS.js
+++ webapp/common/js/includeCSS.js
@@ -1,6 +1,6 @@
 
 //版本管理获取最新版本号进行对比,如果变化则强制刷新页面
-var jwzhVersion = '1.0.0.5';                       //项目更新时手动修改版本号
+var jwzhVersion = '1.1.0.8';                       //项目更新时手动修改版本号
 var staticPath = 'http://static.jwzh.com:7777/jwzh';//注意:请同步修改pathConfig.js中的路径!!!
 var pathname = location.pathname;
 
