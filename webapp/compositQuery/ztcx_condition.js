0b6e41524991a200fe4402327bbd3bda477594d1
zhang.hai <zhang.hai@founder.com.cn>
2017-08-03 14:27:56 +0800

    专题查询
    Signed-off-by: zhang.hai <zhang.hai@founder.com.cn>

---
+++ webapp/compositQuery/ztcx_condition.js
@@ -0,0 +1,4 @@
+var condition0 = [{"id":"=","text":"等于"},{"id":"<>","text":"不等于"}];
+var condition1 = [{"id":"=","text":"等于"},{"id":"<>","text":"不等于"},{"id":"IN","text":"包含"},{"id":"NL","text":"为空"},{"id":"NN","text":"不为空"}];
+var condition2 = [{"id":"=","text":"等于"},{"id":"<>","text":"不等于"},{"id":"NL","text":"为空"},{"id":"NN","text":"不为空"}];
+var condition3 = [{"id":"=","text":"等于"},{"id":">","text":"大于"},{"id":">=","text":"大于等于"},{"id":"<","text":"小于"},{"id":"<=","text":"小于等于"}];
